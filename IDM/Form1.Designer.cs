06ff1fab13f8260129dccbe075439e72224ef15e
wael <wael.derwish71@gmail.com>
2022-11-04 18:41:51 +0300

    internet download manager consol application

---
+++ IDM/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace IDM
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.pathBox = new System.Windows.Forms.TextBox();
+            this.selectPathbtn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.downloadBtn = new System.Windows.Forms.Button();
+            this.puseBtn = new System.Windows.Forms.Button();
+            this.resumeBtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // urlBox
+            // 
+            this.urlBox.Location = new System.Drawing.Point(13, 13);
+            this.urlBox.Name = "urlBox";
+            this.urlBox.Size = new System.Drawing.Size(487, 23);
+            this.urlBox.TabIndex = 0;
+            // 
+            // pathBox
+            // 
+            this.pathBox.Location = new System.Drawing.Point(13, 43);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.Size = new System.Drawing.Size(442, 23);
+            this.pathBox.TabIndex = 1;
+            // 
+            // selectPathbtn
+            // 
+            this.selectPathbtn.Location = new System.Drawing.Point(461, 42);
+            this.selectPathbtn.Name = "selectPathbtn";
+            this.selectPathbtn.Size = new System.Drawing.Size(39, 23);
+            this.selectPathbtn.TabIndex = 2;
+            this.selectPathbtn.Text = "..";
+            this.selectPathbtn.UseVisualStyleBackColor = true;
+            this.selectPathbtn.Click += new System.EventHandler(this.selectPathbtn_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 116);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(487, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(461, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0.00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(461, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "السرعة";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(381, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "0.00";
+            // 
+            // downloadBtn
+            // 
+            this.downloadBtn.Location = new System.Drawing.Point(12, 183);
+            this.downloadBtn.Name = "downloadBtn";
+            this.downloadBtn.Size = new System.Drawing.Size(141, 31);
+            this.downloadBtn.TabIndex = 8;
+            this.downloadBtn.Text = "تنزيل";
+            this.downloadBtn.UseVisualStyleBackColor = true;
+            this.downloadBtn.Click += new System.EventHandler(this.downloadBtn_Click);
+            // 
+            // puseBtn
+            // 
+            this.puseBtn.Location = new System.Drawing.Point(185, 183);
+            this.puseBtn.Name = "puseBtn";
+            this.puseBtn.Size = new System.Drawing.Size(141, 31);
+            this.puseBtn.TabIndex = 9;
+            this.puseBtn.Text = "ايقاف";
+            this.puseBtn.UseVisualStyleBackColor = true;
+            this.puseBtn.Click += new System.EventHandler(this.puseBtn_Click);
+            // 
+            // resumeBtn
+            // 
+            this.resumeBtn.Location = new System.Drawing.Point(359, 183);
+            this.resumeBtn.Name = "resumeBtn";
+            this.resumeBtn.Size = new System.Drawing.Size(141, 31);
+            this.resumeBtn.TabIndex = 10;
+            this.resumeBtn.Text = "اسنئناف";
+            this.resumeBtn.UseVisualStyleBackColor = true;
+            this.resumeBtn.Click += new System.EventHandler(this.resumeBtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(443, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "تم التحميل";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(381, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "0.00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 231);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.resumeBtn);
+            this.Controls.Add(this.puseBtn);
+            this.Controls.Add(this.downloadBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.selectPathbtn);
+            this.Controls.Add(this.pathBox);
+            this.Controls.Add(this.urlBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlBox;
+        private System.Windows.Forms.TextBox pathBox;
+        private System.Windows.Forms.Button selectPathbtn;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button downloadBtn;
+        private System.Windows.Forms.Button puseBtn;
+        private System.Windows.Forms.Button resumeBtn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ IDM/Form1.cs
@@ -0,0 +1,74 @@
+﻿using AltoHttp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IDM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void selectPathbtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser  = new FolderBrowserDialog();
+            if(folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                pathBox.Text = folderBrowser.SelectedPath;
+            }
+        }
+        private void downloadBtn_Click(object sender, EventArgs e)
+        {
+            Download(urlBox.Text, pathBox.Text);
+        }
+        HttpDownloader downloader;
+
+        private void Download (string url,string path)
+        {
+            if (pathBox.Text == "")
+            {
+                MessageBox.Show("الرجاء ادخال مسار التحميل");
+                return;
+            }
+
+            downloader = new HttpDownloader(url,$"{path}\\{Path.GetFileName(url)}");
+            downloader.DownloadCompleted += Downloader_DownloadCompleted;
+            downloader.ProgressChanged += Downloader_ProgressChanged;
+            downloader.Start();
+        }
+
+        private void Downloader_ProgressChanged(object sender, AltoHttp.ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = (int) e.Progress;
+            label2.Text = $"{e.Progress.ToString("0.00")}%";
+            label4.Text = String.Format("{0} KB/S" , (e.SpeedInBytes /1024).ToString("0.00"));
+            label6.Text = String.Format("{0} MB", (downloader.TotalBytesReceived / 1024 / 1024).ToString("0.00"));
+            label1.Text = "جاري التحميل";
+        }
+
+        private void Downloader_DownloadCompleted(object sender, EventArgs e)
+        {
+            label1.Text = "تم التحميل";
+        }
+
+        private void puseBtn_Click(object sender, EventArgs e)
+        {
+            downloader.Pause();
+        }
+
+        private void resumeBtn_Click(object sender, EventArgs e)
+        {
+            downloader.Resume();
+        }
+    }
+}
